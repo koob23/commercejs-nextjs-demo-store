c9d46dd6f5d88714300752e63e4608cfe3cf87d0
koob23 <150365677+koob23@users.noreply.github.com>
2023-11-10 07:35:11 -0600

    Update _document.js
    adding Tealium script

--- pages/_document.js
+++ pages/_document.js
@@ -43,6 +43,78 @@ class MyDocument extends Document {
           <meta name="twitter:card" content="summary_large_image" />
         </Head>
         <body>
+              <script type="text/javascript">
+var utag_data = {
+  page_name : "", // Contains a user-friendly name for the page.
+  page_section : "", // Contains a user-friendly page section, e.g. 'configuration section'.
+  page_category : "", // Contains a user-friendly page category, e.g. 'appliance page'.
+  page_subcategory : "", // Contains a user-friendly page subcategory, e.g. 'refrigerator page'.
+  page_type : "", // Contains a user-friendly page type, e.g. 'cart page'.
+  customer_id : "", // Contains the customer's ID.
+  customer_username : "", // Contains the customer's username, e.g. johndoe38.
+  customer_status : "", // Contains the customer's status, e.g. 'logged in'.
+  customer_type : "", // Contains the customer's type, e.g. 'bronze member'.
+  customer_email : "", // Contains the customer's email.
+  customer_city : "", // Contains the city portion of the customer's address.
+  customer_state : "", // Contains the state portion of the customer's address.
+  customer_zip : "", // Contains the zip code portion of the customer's address.
+  customer_country : "", // Contains the country portion of the customer's address.
+  search_term : "", // Contains the search term or query submitted by a visitor.
+  search_results : "", // Contains the number of results returned with a search query.
+  search_type : "", // Contains the type of search conducted by the visitor.
+  product_id : "", // Contains product ID(s) - multiple values should be comma-separated.
+  product_sku : "", // Contains product SKU(s) - multiple values should be comma-separated.
+  product_name : "", // Contains product name(s) - multiple values should be comma-separated.
+  product_brand : "", // Contains product brand(s) - multiple values should be comma-separated.
+  product_category : "", // Contains product category(s) - multiple values should be comma-separated.
+  product_subcategory : "", // Contains product subcategory(s) - multiple values should be comma-separated.
+  product_unit_price : "", // Contains product unit price(s) - multiple values should be comma-separated.
+  product_list_price : "", // Contains product list price(s) before discount - multiple values should be comma-separated.
+  product_quantity : "", // Contains product quantity(s) - multiple values should be comma-separated.
+  product_discount : "", // Contains product discount(s) - multiple values should be comma-separated.
+  order_id : "", // Contains the order or transaction ID.
+  order_total : "", // Contains the order total value.
+  order_subtotal : "", // Contains the order subtotal (not containing taxes &amp; shipping).
+  order_payment_type : "", // Contains the order payment type.
+  order_discount : "", // Contains the order discount (if any).
+  order_shipping : "", // Contains the order shipping value.
+  order_tax : "", // Contains the order tax amount.
+  order_currency : "", // Contains the currency associated with the transaction, e.g. 'USD'.
+  order_coupon_code : "", // Contains the order coupon code.
+  order_coupon_discount : "", // Contains the order coupon discount.
+  order_store : "", // Contains identification information for a store.
+  order_type : "", // Contains type of order/cart.
+  site_region : "", // Contains values for the region or localized version, e.g. 'en_US'.
+  article_id : "", // Contains the article's ID.
+  article_name : "", // Contains the article's name.
+  article_author : "", // Contains the name of the author of an article.
+  article_publish_date : "", // Contains the publish date of an article.
+  video_id : "", // Contains the video ID.
+  video_name : "", // Contains the name of the video.
+  video_duration : "", // Contains the duration of the video. This value should be in seconds.
+  video_offset : "", // Contains the time into a video at which a certain state occurs.
+  video_state : "", // Contains the state of the video, e.g. start, play, pause, milestone, end.
+  video_player : "", // Contains the name of the video player.
+  video_player_version : "", // Contains the version of the video player.
+  event_type : "", // Contains the event type, e.g. link click', 'button click').
+  event_target : "", // Contains the name associated with an interactive element (link, button, etc).
+  event_attr1 : "", // Contains custom attribute 1 associated with an event.
+  event_attr2 : "", // Contains custom attribute 2 associated with an event.
+  event_attr3 : "", // Contains custom attribute 3 associated with an event.
+  event_attr4 : "" // Contains custom attribute 4 associated with an event.
+}
+</script>
+
+<!-- Loading script asynchronously -->
+<script type="text/javascript">
+    (function(a,b,c,d){
+    a='https://tags.tiqcdn.com/utag/blastam-training/abos/prod/utag.js';
+    b=document;c='script';d=b.createElement(c);d.src=a;d.type='text/java'+c;d.async=true;
+    a=b.getElementsByTagName(c)[0];a.parentNode.insertBefore(d,a);
+    })();
+</script>
+
+    
           <Main />
           <NextScript />
         </body>
